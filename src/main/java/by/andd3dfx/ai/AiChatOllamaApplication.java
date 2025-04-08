b7db11cc8d010fd5e9a56bc1f61532edca04fab8
Andrei Punko <andd3dfx@gmail.com>
2025-04-09 02:16:05 +0300

    Add Spring Boot project skeleton

---
+++ src/main/java/by/andd3dfx/ai/AiChatOllamaApplication.java
@@ -0,0 +1,13 @@
+package by.andd3dfx.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiChatOllamaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiChatOllamaApplication.class, args);
+	}
+
+}
