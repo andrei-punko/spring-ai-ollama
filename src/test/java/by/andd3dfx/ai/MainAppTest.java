933c90e60a5dfa954fd7acee2f6bb7d20a51f473
Andrei Punko <andd3dfx@gmail.com>
2025-04-14 17:13:03 +0300

    Add test to make call to Ollama. Extract spring-boot test into separate class

--- src/test/java/by/andd3dfx/ai/MainAppTest.java
+++ src/test/java/by/andd3dfx/ai/MainAppTest.java
@@ -2,69 +2,12 @@ package by.andd3dfx.ai;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.testcontainers.ollama.OllamaContainer;
-import org.testcontainers.utility.DockerImageName;
-
-import java.io.IOException;
-
-import static io.restassured.RestAssured.given;
-import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class MainAppTest {
 
-    private final String OLLAMA_IMAGE_VERSION = "0.6.5";
-    private final String OLLAMA_IMAGE_NAME = "ollama/ollama:" + OLLAMA_IMAGE_VERSION;
-
     @Test
-    public void startOllama() {
-        try (var ollama = new OllamaContainer(OLLAMA_IMAGE_NAME)) {
-            ollama.start();
-
-            checkApiVersion(ollama);
-        }
-    }
-
-    private void checkApiVersion(OllamaContainer ollama) {
-        String apiVersion = given()
-                .baseUri(ollama.getEndpoint())
-                .get("/api/version")
-                .jsonPath().get("version");
-        assertThat(apiVersion).isEqualTo(OLLAMA_IMAGE_VERSION);
-    }
-
-    @Test
-    public void startOllamaDownloadModelAndCommitToImage() throws IOException, InterruptedException {
-        String newImageName = "tc-ollama-all-minilm";
-        try (var ollama = new OllamaContainer(OLLAMA_IMAGE_NAME)) {
-            ollama.start();
-
-            // pull model
-            ollama.execInContainer("ollama", "pull", "all-minilm");
-
-            checkModelName(ollama);
-
-            // commit to image
-            ollama.commitToImage(newImageName);
-        }
-        try (   // substitute
-                var ollama = new OllamaContainer(
-                        DockerImageName.parse(newImageName)
-                                .asCompatibleSubstituteFor(OLLAMA_IMAGE_NAME)
-                )
-        ) {
-            ollama.start();
-
-            checkModelName(ollama);
-        }
-    }
-
-    private void checkModelName(OllamaContainer ollama) {
-        String modelName = given()
-                .baseUri(ollama.getEndpoint())
-                .get("/api/tags")
-                .jsonPath()
-                .getString("models[0].name");
-        assertThat(modelName).contains("minilm");
+    void contextLoads() {
+        // do nothing, this test for just check is context up
     }
 }
---
+++ src/test/java/by/andd3dfx/ai/ollama/OllamaContainerTest.java
@@ -0,0 +1,69 @@
+package by.andd3dfx.ai.ollama;
+
+import org.junit.jupiter.api.Test;
+import org.testcontainers.ollama.OllamaContainer;
+import org.testcontainers.utility.DockerImageName;
+
+import java.io.IOException;
+
+import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class OllamaContainerTest {
+
+    public static final String OLLAMA_IMAGE_VERSION = "0.6.5";
+    public static final String OLLAMA_IMAGE_NAME = "ollama/ollama:" + OLLAMA_IMAGE_VERSION;
+    public static String TINYLLAMA_MODEL_NAME = "tinyllama";
+    public static String TINYLLAMA_IMAGE_NAME = "tc-ollama-" + TINYLLAMA_MODEL_NAME;
+
+    @Test
+    public void startOllama() {
+        try (var ollama = new OllamaContainer(OLLAMA_IMAGE_NAME)) {
+            ollama.start();
+
+            checkApiVersion(ollama);
+        }
+    }
+
+    private void checkApiVersion(OllamaContainer ollama) {
+        String apiVersion = given()
+                .baseUri(ollama.getEndpoint())
+                .get("/api/version")
+                .jsonPath().get("version");
+        assertThat(apiVersion).isEqualTo(OLLAMA_IMAGE_VERSION);
+    }
+
+    @Test
+    public void startOllamaDownloadModelAndCommitToImage() throws IOException, InterruptedException {
+        try (var ollama = new OllamaContainer(OLLAMA_IMAGE_NAME)) {
+            ollama.start();
+
+            // pull model
+            ollama.execInContainer("ollama", "pull", TINYLLAMA_MODEL_NAME);
+
+            checkModelName(ollama);
+
+            // commit to image
+            ollama.commitToImage(TINYLLAMA_IMAGE_NAME);
+        }
+        try (   // substitute
+                var ollama = new OllamaContainer(
+                        DockerImageName.parse(TINYLLAMA_IMAGE_NAME)
+                                .asCompatibleSubstituteFor(OLLAMA_IMAGE_NAME)
+                )
+        ) {
+            ollama.start();
+
+            checkModelName(ollama);
+        }
+    }
+
+    private void checkModelName(OllamaContainer ollama) {
+        String modelName = given()
+                .baseUri(ollama.getEndpoint())
+                .get("/api/tags")
+                .jsonPath()
+                .getString("models[0].name");
+        assertThat(modelName).contains(TINYLLAMA_MODEL_NAME);
+    }
+}
